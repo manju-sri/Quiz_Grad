f9d80a692f0de259348d4c63393eb0131e96dde9
manju-sri <107233576+manju-sri@users.noreply.github.com>
2024-01-27 23:42:58 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,8 @@
+
+const startBtn1 = document.getElementById("start-btn1");
+const con2 = document.getElementById("container-2");
+
+startBtn1.addEventListener("click", ()=>{
+    startBtn1.classList.remove("hide-class");
+    startBtn1.classList.add("show-class");
+});
